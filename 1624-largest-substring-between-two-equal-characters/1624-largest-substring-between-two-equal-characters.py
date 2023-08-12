cbb7a3fa343af5d6d235f9bb147f22294161c076
Septio Nugroho <septio.nugroho@ui.ac.id>
2023-08-12 14:21:11 +0700

    Time: 42 ms (77.70%), Space: 16.3 MB (34.07%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
@@ -9,14 +9,12 @@ class Solution:
             if idx != -1:
                 indexes[s[i]].append(idx)
 
-        max_len = 0
-        is_available = False
+        max_len = -1
         for key, value in indexes.items():
             if len(value) >= 2:
-                is_available = True
                 diff = value[-1] - value[0] - 1
                 max_len = diff if diff > max_len else max_len
 
-        return max_len if is_available else -1
+        return max_len
             
             
