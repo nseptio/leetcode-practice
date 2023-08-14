ff5c884263caddb45b745b01d4a669334f037026
Septio Nugroho <septio.nugroho@ui.ac.id>
2023-08-14 18:33:53 +0700

    Time: 32 ms (96.15%), Space: 16.3 MB (60.92%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,5 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        fahrenheit = (celsius * 9/5) + 32
+        return [round(kelvin, 5), round(fahrenheit, 5)]
