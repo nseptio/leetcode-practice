d845c0efd8fa6c8f23aff689cdbc1a6a5ee4e2f0
Septio Nugroho <septio.nugroho@ui.ac.id>
2023-12-24 11:14:46 +0700

    Time: 6 ms (77.05%), Space: 4.2 MB (19.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -2,9 +2,9 @@ func twoSum(nums []int, target int) []int {
 	numIndex := make(map[int]int)
 	for index, value := range nums {
 		complement := target - value
-		_, ok := numIndex[complement]
+		mapValue, ok := numIndex[complement]
 		if ok {
-			return []int{numIndex[complement], index}
+			return []int{mapValue, index}
 		}
 		numIndex[value] = index
 	}
