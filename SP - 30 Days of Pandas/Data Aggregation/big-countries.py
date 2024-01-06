394ce5468ce395149d29ead6fd6bcf17db717012
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-01-06 13:46:33 +0700

    Delete SP - 30 Days of Pandas/Data Aggregation directory

--- SP - 30 Days of Pandas/Data Aggregation/big-countries.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    df = world[["name", "population", "area"]]
-
-    return df[(df["area"] >= 3000000) | (df["population"] >= 25000000)]
