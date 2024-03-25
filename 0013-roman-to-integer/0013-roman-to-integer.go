ab224661908dcd549c595f4b196d21a85dfc8d0b
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-03-25 11:03:51 +0700

    Time: 0 ms (100.00%), Space: 3 MB (14.04%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.go
+++ 0013-roman-to-integer/0013-roman-to-integer.go
@@ -11,14 +11,8 @@ func romanToInt(s string) int {
     chars := []rune(s)
     result := 0
 	for i := 0; i < len(chars); i++ {
-		if i == len(chars)-1 {
-			result += romanMap[chars[i]]
-			break
-		}
-
-		if romanMap[chars[i]] < romanMap[chars[i+1]] {
-			result += romanMap[chars[i+1]] - romanMap[chars[i]]
-			i++
+        if i < len(chars) - 1 && romanMap[chars[i]] < romanMap[chars[i+1]] {
+			result -= romanMap[chars[i]]
 		} else {
 			result += romanMap[chars[i]]
 		}
