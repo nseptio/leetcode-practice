c16fb916eaed9717f69b157ae663f56dae954490
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-09-13 14:39:08 +0700

    Time: 5 ms (35.29%), Space: 3.6 MB (75.00%) - LeetHub

---
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.go
@@ -0,0 +1,11 @@
+import "strings"
+
+func countPrefixes(words []string, s string) int {
+	result := 0
+	for i := 0; i < len(words); i++ {
+		if strings.HasPrefix(s, words[i]) {
+			result++
+		}
+	}
+	return result
+}
