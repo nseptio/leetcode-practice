97d01cb8914807c5bce6fb71c1b763ab5fa93b09
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-09-14 20:36:04 +0700

    Time: 1 ms (67.06%), Space: 2.2 MB (64.71%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.go
@@ -0,0 +1,14 @@
+func numberOfMatches(n int) int {
+    result := 0
+    for n > 1 {
+        if (n % 2 == 0) {
+            result += n/2
+            n = n/2
+        } else {
+            result += (n-1) / 2
+            n = ((n-1) / 2) + 1
+        }
+    }
+    
+    return result
+}
