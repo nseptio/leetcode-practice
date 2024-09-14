1eeb629be6231e280c9c3b2281c83f188c71300f
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-09-14 20:38:27 +0700

    Time: 2 ms (8.24%), Space: 2.1 MB (100.00%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.go
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.go
@@ -1,7 +1,7 @@
 func numberOfMatches(n int) int {
     result := 0
     for n > 1 {
-        if (n % 2 == 0) {
+        if ((n&1) == 0) {
             result += n/2
             n = n/2
         } else {
