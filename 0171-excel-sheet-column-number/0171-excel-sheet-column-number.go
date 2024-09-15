ffd038a0bb388c5d0803926c2f95c23f133c037b
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-09-15 10:38:10 +0700

    Time: 0 ms (100.00%), Space: 2.4 MB (17.51%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.go
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.go
@@ -1,10 +1,7 @@
-import "math"
-
 func titleToNumber(columnTitle string) int {
     result := 0
     for i:= 0; i < len(columnTitle); i++ {
-        value := int(columnTitle[i] - 'A') + 1
-        result += value * int(math.Pow(26, float64(len(columnTitle) - i - 1)))
+        result = result * 26 + int(columnTitle[i] - 'A') + 1
     }
     return result
 }
