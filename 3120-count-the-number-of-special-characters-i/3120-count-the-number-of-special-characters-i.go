fdcf360f0745e5d9693e7f1169abaeee2e4c2026
Septio Nugroho <septio.nugroho@ui.ac.id>
2024-12-14 06:42:09 +0700

    Time: 0 ms (100.00%), Space: 4.4 MB (11.54%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.go
@@ -0,0 +1,25 @@
+import "unicode"
+
+func numberOfSpecialChars(word string) int {
+
+    upperChar := make(map[rune]bool, len(word))
+    lowerChar := make(map[rune]bool, len(word))
+    result := 0
+    for _, r := range word {
+        if unicode.IsLower(r) {
+            lowerChar[r] = true
+        } else {
+            upperChar[r] = true
+        }
+    }
+    
+    for k, _ := range lowerChar {
+        _, ok := upperChar[unicode.ToUpper(k)]
+        
+        if ok {
+            result+=1
+        }
+    }
+    
+    return result
+}
