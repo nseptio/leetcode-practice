810d5444ef78ada5ac319b72f6109f7fa25c3168
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-04-04 20:41:20 +0700

    Time: 0 ms (100%), Space: 4.1 MB (24.02%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.go
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.go
@@ -1,14 +1,27 @@
 func areAlmostEqual(s1 string, s2 string) bool {
-    diff := 0
-    for i := 0; i < len(s1); i++ {
-        if s1[i] != s2[i] {
-            diff++
-        }
-    }
+	s1Map := make(map[byte]int)
+	s2Map := make(map[byte]int)
+	for i := 0; i < len(s1); i++ {
+		s1Map[s1[i]]++
+		s2Map[s2[i]]++
+	}
 
-    if diff > 2 {
-        return false
-    }
+	for k, v := range s1Map {
+		if s2Map[k] != v {
+			return false
+		}
+	}
 
-    return true
+	diff := 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			diff++
+		}
+	}
+
+	if diff > 2 {
+		return false
+	}
+
+	return true
 }
