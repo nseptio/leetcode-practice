b8c10da60d974df0292d32f37f3017a832a7f147
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-08-03 07:40:13 +0700

    0182-DUPLICATE-EMAILS Stats: Time: 262 ms (85.74%), Space: 67 MB (80.74%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
 
-    person = person[person.duplicated(subset='email', keep='first')]
-    df_unique_email = person.drop_duplicates(subset='email')
+    df_duplicated_email = person[person.duplicated(subset='email', keep='first')]
+    df_unique_email = df_duplicated_email.drop_duplicates(subset='email')
     return df_unique_email[['email']]
