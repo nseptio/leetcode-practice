6901a31f2fabd95d853439d7d2f4d125f44f32e3
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-08-07 09:46:51 +0700

    0607-SALES-PERSON Stats: Time: 331 ms (96.81%), Space: 67.7 MB (93.02%) - LeetHub

--- 0607-sales-person/0607-sales-person.py
+++ 0607-sales-person/0607-sales-person.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    return sales_person[
-        ~sales_person["sales_id"]
-            .isin(
-                company[company["name"] == "RED"]
-                    .merge(orders[["sales_id", "com_id"]], on='com_id')["sales_id"]
-            )
-        ][["name"]]
+    company_red_id = company[company['name'] == 'RED']["com_id"]
+    order_red = orders[orders['com_id'].isin(company_red_id)]['sales_id']
+
+    return sales_person[~sales_person['sales_id'].isin(order_red)][['name']]
