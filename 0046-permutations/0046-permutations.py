bd22b7404fc5eea68569fb6b6aefbb44cafb3082
Septio Nugroho <septio.nugroho@ui.ac.id>
2026-04-20 08:19:34 +0700

    0046-PERMUTATIONS Stats: Time: 0 ms (100%), Space: 19.4 MB (88.87%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res, curr = [], []
+        if len(nums) == 0:
+            return [[]]
+        
+        perms = self.permute(nums[1:])
 
-        def dfs():
-            if len(curr) == len(nums):
-                res.append(curr.copy())
-                return
-            
-            for i in nums:
-                if i not in curr:
-                    curr.append(i)
-                    dfs()
-                    curr.pop()
+        res = []
+        for p in perms:
+            for i in range(0, len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
         
-        dfs()
         return res
-        
